bb29e18ba3547fb2f23d3134ca6afc8255a65d5f
T3Marius <dumitrascumarius077@gmail.com>
2026-07-02 13:37:17 +0300

    (core): bumped to v0.1.7

--- Jailbreak/src/Jailbreak.cs
+++ Jailbreak/src/Jailbreak.cs
@@ -13,7 +13,7 @@ namespace Jailbreak;
     Name = "Jailbreak",
     Id = "Jailbreak",
     Author = "Marius",
-    Version = "0.1.6"
+    Version = "0.1.7"
 )]
 public sealed class Main : BasePlugin
 {
